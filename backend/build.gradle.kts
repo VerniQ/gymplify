8bc76d78ae17d2b019b74eb392758f8b18887d84
Verni <vernidev@gmail.com>
2025-04-01 11:01:36 +0200

    Initialize backend.
    Add dependencies.
    Add basic config.
    Create project structure.

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.2.0"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "me.verni.gymplify"
@@ -12,6 +14,32 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // REST API
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // Security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    // Validation
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-impl:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.11.5")
+
+    // JDBC
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+
+    //Oracle JDBC Driver
+    runtimeOnly("com.oracle.database.jdbc:ojdbc11:23.3.0.23.09")
+
+    // Test
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // Security Test
+    testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.test {
---
+++ backend/src/main/java/me/verni/gymplify/Gymplify.java
@@ -0,0 +1,10 @@
+package me.verni.gymplify;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gymplify {
+    public static void main(String[] args){
+        System.out.println("Gymplify app!");
+    }
+}
